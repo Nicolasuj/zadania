dc5c657fbfbe1155ed307b2080bd87d9a644dd29
Nicolasuj <frendzle@gmail.com>
2026-05-24 15:52:11 +0200

    Add files via upload

--- zadania.ts
+++ zadania.ts
@@ -1,23 +1,23 @@
 
 // zadanie 1
 
-function reversedNumber(number: number) {
-    let reversedNumber = number.toString().split('').reverse().join('');
+// function reversedNumber(number: number) {
+//     let reversedNumber = number.toString().split('').reverse().join('');
 
-    return Number(reversedNumber);
-}
+//     return Number(reversedNumber);
+// }
 
-console.log(reversedNumber(1234));
+// console.log(reversedNumber(1234));
 
 // zadanie 6
 
-let myColor = ['Red', 'Green', 'White', 'Black'];
+// let myColor = ['Red', 'Green', 'White', 'Black'];
 
-function joinArrayElements(array: string[]) {
-    return console.log(array.join()), console.log(array.join()), console.log(array.join('+'))
-}
+// function joinArrayElements(array: string[]) {
+//     return console.log(array.join()), console.log(array.join()), console.log(array.join('+'))
+// }
 
-joinArrayElements(myColor)
+// joinArrayElements(myColor)
 
 // zadanie 10
 // Write a JavaScript function to split a string and convert it into an array of words.
@@ -25,11 +25,11 @@ joinArrayElements(myColor)
 // console.log(string_to_array("Robin Singh"));
 // ["Robin", "Singh"]
 
-function string_to_array(string: string) {
-    return console.log(string.split(' '))
-}
+// function string_to_array(string: string) {
+//     return console.log(string.split(' '))
+// }
 
-string_to_array('Robin Singh')
+// string_to_array('Robin Singh')
 
 // zadanie 11
 // Write a JavaScript function to concatenate a given string n times (default is 1).
@@ -41,3 +41,10 @@ string_to_array('Robin Singh')
 // "Ha!Ha!"
 // "Ha!Ha!Ha!"
 
+function repeat(string: string, n?: number) {
+    
+    if (n >= 2) {
+        return string.repeat(n)
+    } else return string
+
+}
